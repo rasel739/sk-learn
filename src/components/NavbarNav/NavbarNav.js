e7fcb531cca8bfcd5ca1928370d511771f139f6c
rasel739 <raselhossaintro@gmail.com>
2021-10-04 00:43:38 +0600

    start assignment

---
+++ src/components/NavbarNav/NavbarNav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NavbarNav.css';
+
+const NavbarNav = () => {
+    return (
+        <div>
+            <h1>Navbar Nav</h1>
+        </div>
+    );
+};
+
+export default NavbarNav;
